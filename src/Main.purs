8fdda6db620430446bfe5e4a9e0405f673d39f33
bouzuya <m@bouzuya.net>
2019-02-16 22:21:55 +0900

    add --template option

--- src/Main.purs
+++ src/Main.purs
@@ -13,11 +13,16 @@ import Prelude (Unit, bind, discard, map, mempty, pure, (<<<), (<>))
 import Simple.JSON as SimpleJSON
 
 type OptionsRecord =
-  { directory :: Maybe String }
+  { directory :: Maybe String
+  , template :: Maybe String
+  }
 data CommandLineOptions
 
 directoryOption :: Option CommandLineOptions String
 directoryOption = Options.opt "directory"
+
+templateOption :: Option CommandLineOptions String
+templateOption = Options.opt "template"
 
 optionsToRecord :: Options CommandLineOptions -> Maybe OptionsRecord
 optionsToRecord = SimpleJSON.read_ <<< Options.options
@@ -28,6 +33,7 @@ parseOptions args = Tuple.snd (Array.foldl go (Tuple Nothing mempty) args)
     go (Tuple Nothing options) arg =
       case arg of
         "--directory" -> Tuple (Just directoryOption) options
+        "--template" -> Tuple (Just templateOption) options
         _ -> Tuple Nothing options
     go (Tuple (Just option) options) arg =
       Tuple Nothing (options <> Options.assoc option arg)
