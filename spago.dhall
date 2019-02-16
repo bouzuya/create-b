df04883fe4f2cab15e3817567307dab2bfae8c50
bouzuya <m@bouzuya.net>
2019-02-16 22:17:17 +0900

    add directory option

--- spago.dhall
+++ spago.dhall
@@ -5,7 +5,14 @@ You can edit this file as you like.
 { name =
     "my-project"
 , dependencies =
-    [ "console", "effect", "node-process", "psci-support", "test-unit" ]
+    [ "console"
+    , "effect"
+    , "node-process"
+    , "options"
+    , "psci-support"
+    , "simple-json"
+    , "test-unit"
+    ]
 , packages =
     ./packages.dhall
 }
--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,40 @@
 module Main where
 
 import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Data.Options (Option, Options(..))
+import Data.Options as Options
+import Data.Tuple (Tuple(..))
+import Data.Tuple as Tuple
 import Effect (Effect)
 import Effect.Class.Console as Console
 import Node.Process as Process
-import Prelude (Unit, bind, map)
+import Prelude (Unit, bind, discard, map, mempty, pure, (<<<), (<>))
+import Simple.JSON as SimpleJSON
+
+type OptionsRecord =
+  { directory :: Maybe String }
+data CommandLineOptions
+
+directoryOption :: Option CommandLineOptions String
+directoryOption = Options.opt "directory"
+
+optionsToRecord :: Options CommandLineOptions -> Maybe OptionsRecord
+optionsToRecord = SimpleJSON.read_ <<< Options.options
+
+parseOptions :: Array String -> Options CommandLineOptions
+parseOptions args = Tuple.snd (Array.foldl go (Tuple Nothing mempty) args)
+  where
+    go (Tuple Nothing options) arg =
+      case arg of
+        "--directory" -> Tuple (Just directoryOption) options
+        _ -> Tuple Nothing options
+    go (Tuple (Just option) options) arg =
+      Tuple Nothing (options <> Options.assoc option arg)
 
 main :: Effect Unit
 main = do
   args <- map (Array.drop 2) Process.argv
   Console.logShow args
+  options <- pure ((optionsToRecord <<< parseOptions) args)
+  Console.logShow options
