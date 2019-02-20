f5435a7768b4d924f57b1043280b10e487e11fa1
bouzuya <m@bouzuya.net>
2019-02-20 14:36:16 +0900

    extract TemplateVariables module

--- src/Main.purs
+++ src/Main.purs
@@ -2,34 +2,21 @@ module Main
   ( main
   ) where
 
-import Bouzuya.DateTime (Date, DateTime(..))
-import Bouzuya.DateTime as DateTime
 import Data.Array as Array
-import Data.Date as Date
-import Data.Formatter.DateTime as Formatter
-import Data.Int as Int
-import Data.List as List
 import Data.Maybe (Maybe(..), maybe)
 import Data.Options (Option, Options)
 import Data.Options as Options
-import Data.Time.Duration (Days(..))
-import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..))
 import Data.Tuple as Tuple
-import DateTimeFormatter as DateTimeFormatter
 import Effect (Effect)
 import Effect.Exception (throw)
-import Effect.Now (nowDateTime)
-import Foreign.Object as Object
 import Node.Encoding as Encoding
 import Node.FS.Sync as FS
 import Node.Process as Process
-import OffsetDateTime as OffsetDateTime
-import Prelude (class Show, Unit, bind, bottom, discard, map, mempty, negate, pure, (<<<), (<>))
+import Prelude (class Show, Unit, bind, discard, map, mempty, pure, (<<<), (<>))
 import Simple.JSON as SimpleJSON
 import TemplateString as TemplateString
-import TimeZoneOffset as TimeZoneOffset
-import WeekDate as WeekDate
+import TemplateVariables as TemplateVariables
 
 data Template
   = BlogPostWeekday
@@ -73,33 +60,8 @@ parseOptions args = Tuple.snd (Array.foldl go (Tuple Nothing mempty) args)
     go (Tuple (Just option) options) arg =
       Tuple Nothing (options <> Options.assoc option arg)
 
-pathFormatter :: Formatter.Formatter
-pathFormatter =
-  List.fromFoldable
-    [ Formatter.Placeholder "/"
-    , Formatter.YearFull
-    , Formatter.Placeholder "/"
-    , Formatter.MonthTwoDigits
-    , Formatter.Placeholder "/"
-    , Formatter.YearFull
-    , Formatter.Placeholder "-"
-    , Formatter.MonthTwoDigits
-    , Formatter.Placeholder "-"
-    , Formatter.DayOfMonthTwoDigits
-    ]
-
-formatPath :: Date -> String
-formatPath d = Formatter.format pathFormatter (DateTime d bottom)
-
 readTemplate :: String -> Effect String
 readTemplate s = FS.readTextFile Encoding.UTF8 ("./templates/" <> s)
-
--- [(d - 1), (d - 2), ..., (d - 7)]
-datesInWeekBefore :: Date -> Array Date
-datesInWeekBefore date =
-  Array.catMaybes do
-    days <- map (Days <<< negate <<< Int.toNumber) (Array.range 1 7)
-    pure (Date.adjust days date)
 
 main :: Effect Unit
 main = do
@@ -115,59 +77,7 @@ main = do
         (Just BlogPostWeekday)
         templateFromString
         options.template)
-  dateTimeInUTC <- nowDateTime
-  inJp <-
-    maybe
-      (throw "invalid time zone offset")
-      pure
-      (TimeZoneOffset.fromString "+09:00")
-  nowInJp <-
-    maybe
-      (throw "invalid offset date time")
-      pure
-      (OffsetDateTime.offsetDateTime inJp dateTimeInUTC)
-  localDateTime <- pure (OffsetDateTime.toDateTime nowInJp)
-  wd <- pure (WeekDate.toWeekDate (DateTime.date localDateTime))
-  let
-    wy = WeekDate.weekYear wd
-    woy = WeekDate.weekOfYear wd
-    dates = datesInWeekBefore (DateTime.date localDateTime)
-  posts <-
-    traverse
-      (\d -> do
-        text <-
-          FS.readTextFile Encoding.UTF8 (directory <> (formatPath d) <> ".json")
-        { title } <-
-          maybe
-            (throw "invalid meta data")
-            pure
-            (SimpleJSON.readJSON_ text :: _ { title :: String })
-        pure { date: DateTimeFormatter.toDateString' d, title })
-      dates
-  let
-    weekPosts =
-      Array.intercalate
-        "\n"
-        (map
-          (\{ date, title } ->
-            TemplateString.template
-              "- [{{date}} {{title}}][{{date}}]"
-              (Object.fromFoldable
-                [ Tuple "date" date
-                , Tuple "title" title
-                ]))
-          posts)
-    templateVariables =
-      Object.fromFoldable
-        [ -- YYYY-MM-DDTHH:MM:SS+09:00
-          Tuple "local_date_time" (OffsetDateTime.toString nowInJp)
-          -- YYYY-Www
-        , Tuple "year_week" (WeekDate.toYearWeekString wd)
-          -- /YYYY/MM/YYYY-MM-DD
-        , Tuple "blog_post_path" (Formatter.format pathFormatter localDateTime)
-          -- - [YYYY-MM-DD title][YYYY-MM-DD]\n...
-        , Tuple "week_posts" weekPosts
-        ]
+  templateVariables <- TemplateVariables.build directory
   generated <-
     case template of
       BlogPostWeekday -> do
---
+++ src/TemplateVariables.purs
@@ -0,0 +1,122 @@
+module TemplateVariables
+  ( build
+  ) where
+
+import Bouzuya.DateTime (Date, DateTime(..))
+import Bouzuya.DateTime as DateTime
+import Data.Array as Array
+import Data.Date as Date
+import Data.Formatter.DateTime as Formatter
+import Data.Int as Int
+import Data.List as List
+import Data.Maybe (maybe)
+import Data.Time.Duration (Days(..))
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
+import DateTimeFormatter as DateTimeFormatter
+import Effect (Effect)
+import Effect.Exception (throw)
+import Effect.Now (nowDateTime)
+import Foreign.Object (Object)
+import Foreign.Object as Object
+import Node.Encoding as Encoding
+import Node.FS.Sync as FS
+import OffsetDateTime (OffsetDateTime)
+import OffsetDateTime as OffsetDateTime
+import Prelude (bind, bottom, map, negate, pure, (<<<), (<>))
+import Simple.JSON as SimpleJSON
+import TemplateString as TemplateString
+import TimeZoneOffset as TimeZoneOffset
+import WeekDate as WeekDate
+
+type Post = { date :: String, title :: String }
+
+build :: String -> Effect (Object String)
+build directory = do
+  nowInJp <- nowOffsetDateTimeInJp
+  posts <- readPosts directory (OffsetDateTime.toDateTime nowInJp)
+  pure (build' nowInJp posts)
+
+build' :: OffsetDateTime -> Array Post -> Object String
+build' nowInJp posts =
+  let
+    wd = WeekDate.toWeekDate (DateTime.date localDateTime)
+    localDateTime = OffsetDateTime.toDateTime nowInJp
+  in
+    Object.fromFoldable
+      [ -- YYYY-MM-DDTHH:MM:SS+09:00
+        Tuple "local_date_time" (OffsetDateTime.toString nowInJp)
+        -- YYYY-Www
+      , Tuple "year_week" (WeekDate.toYearWeekString wd)
+        -- /YYYY/MM/YYYY-MM-DD
+      , Tuple "blog_post_path" (Formatter.format pathFormatter localDateTime)
+        -- - [YYYY-MM-DD title][YYYY-MM-DD]\n...
+      , Tuple "week_posts" (buildWeekPosts posts)
+      ]
+
+buildWeekPosts :: Array Post -> String
+buildWeekPosts posts = Array.intercalate "\n" (map postToLine posts)
+  where
+    lineTmpl = "- [{{date}} {{title}}][{{date}}]"
+    postToVars { date, title } =
+      Object.fromFoldable
+        [ Tuple "date" date
+        , Tuple "title" title
+        ]
+    postToLine post =
+      TemplateString.template lineTmpl (postToVars post)
+
+-- [(d - 1), (d - 2), ..., (d - 7)]
+datesInWeekBefore :: Date -> Array Date
+datesInWeekBefore date =
+  Array.catMaybes do
+    days <- map (Days <<< negate <<< Int.toNumber) (Array.range 1 7)
+    pure (Date.adjust days date)
+
+formatPath :: Date -> String
+formatPath d = Formatter.format pathFormatter (DateTime d bottom)
+
+nowOffsetDateTimeInJp :: Effect OffsetDateTime
+nowOffsetDateTimeInJp = do
+  dateTimeInUTC <- nowDateTime
+  jpOffset <-
+    maybe
+      (throw "invalid time zone offset")
+      pure
+      (TimeZoneOffset.fromString "+09:00")
+  maybe
+    (throw "invalid offset date time")
+    pure
+    (OffsetDateTime.offsetDateTime jpOffset dateTimeInUTC)
+
+pathFormatter :: Formatter.Formatter
+pathFormatter =
+  List.fromFoldable
+    [ Formatter.Placeholder "/"
+    , Formatter.YearFull
+    , Formatter.Placeholder "/"
+    , Formatter.MonthTwoDigits
+    , Formatter.Placeholder "/"
+    , Formatter.YearFull
+    , Formatter.Placeholder "-"
+    , Formatter.MonthTwoDigits
+    , Formatter.Placeholder "-"
+    , Formatter.DayOfMonthTwoDigits
+    ]
+
+readPost :: String -> Date -> Effect Post
+readPost directory d = do
+  let metaPath = directory <> (formatPath d) <> ".json"
+  text <- FS.readTextFile Encoding.UTF8 metaPath
+  { title } <-
+    maybe
+      (throw "invalid meta data")
+      pure
+      (SimpleJSON.readJSON_ text :: _ { title :: String })
+  pure { date: DateTimeFormatter.toDateString' d, title }
+
+readPosts :: String -> DateTime -> Effect (Array Post)
+readPosts directory localDateTime =
+  traverse
+    (readPost directory)
+    (datesInWeekBefore (DateTime.date localDateTime))
