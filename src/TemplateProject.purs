50d0f65e8946f3244eceedb19c00aff5f860d711
bouzuya <m@bouzuya.net>
2019-03-22 19:09:20 +0900

    fix output path

--- src/TemplateProject.purs
+++ src/TemplateProject.purs
@@ -5,24 +5,40 @@ module TemplateProject
 import Prelude
 
 import Bouzuya.TemplateString as TemplateString
+import Data.Maybe as Maybe
+import Data.String as String
+import Data.Traversable as Traversable
 import Effect (Effect)
 import Effect as Effect
+import Effect.Exception as Exception
 import FS as FS
 import Foreign.Object (Object)
 import Node.Path as Path
 
 processDirectory :: String -> String -> Object String -> Effect Unit
-processDirectory outputDirectory inputPath variables = do
-  files <- FS.readDirectory inputPath
-  Effect.foreachE files \file -> do
-    isDirectory <- FS.isDirectory file
-    if isDirectory
-      then processDirectory outputDirectory file variables
-      else processFile outputDirectory file variables
+processDirectory outputDirectory inputDirectory variables = do
+  outputRoot <- Path.resolve [] outputDirectory
+  inputRoot <- Path.resolve [] inputDirectory
+  let
+    go :: String -> Effect Unit
+    go inputPath = do
+      files <- FS.readDirectory inputPath
+      Effect.foreachE files \file -> do
+        isDirectory <- FS.isDirectory file
+        if isDirectory
+          then go file
+          else do
+            file' <-
+              Maybe.maybe
+                (Exception.throw ("invalid directory"))
+                pure
+                (String.stripPrefix (String.Pattern inputRoot) file)
+            processFile outputRoot inputRoot file' variables
+  go inputRoot
 
-processFile :: String -> String -> Object String -> Effect Unit
-processFile outputDirectory inputPath variables = do
-  contentTemplate <- FS.readTextFile inputPath
+processFile :: String -> String -> String -> Object String -> Effect Unit
+processFile outputDirectory inputDirectory inputPath variables = do
+  contentTemplate <- FS.readTextFile (Path.concat [inputDirectory, inputPath])
   let
     pathTemplate = Path.concat [outputDirectory, inputPath]
     outputPath = TemplateString.template pathTemplate variables
