32c1949a103f4bb1456cdeb59e46e05e498330c5
bouzuya <m@bouzuya.net>
2019-04-08 13:05:33 +0900

    Use Bouzuya.DateTime.Formatter.WeekDate

--- src/WeekDateFormat.purs
+++ src/WeekDateFormat.purs
@@ -3,29 +3,15 @@ module WeekDateFormat
   , toYearWeekString
   ) where
 
+import Bouzuya.DateTime.Formatter.WeekDate as WeekDateFormatter
 import Bouzuya.DateTime.WeekDate (WeekDate)
-import Bouzuya.DateTime.WeekDate as WeekDate
-import Data.Enum as Enum
-import Data.Foldable as Foldable
-import Prelude (otherwise, show, (<), (<>))
+import Data.String as String
 
 -- | YYYY-Www-D
 toISOString :: WeekDate -> String
-toISOString wd =
-  toYearWeekString wd <> "-" <> show (Enum.fromEnum (WeekDate.weekday wd))
+toISOString = WeekDateFormatter.toString
 
 -- | YYYY-Www
 toYearWeekString :: WeekDate -> String
 toYearWeekString wd =
-  let
-    y = Enum.fromEnum (WeekDate.weekYear wd)
-    woy = Enum.fromEnum (WeekDate.week wd)
-    pad2 n
-      | n < 10 = "0" <> show n
-      | otherwise = show n
-    pad4 n
-      | n < 10 = "000" <> show n
-      | n < 100 = "00" <> show n
-      | n < 1000 = "0" <> show n
-      | otherwise = show n
-  in Foldable.fold [pad4 y, "-W", pad2 woy]
+  String.take (String.length "YYYY-Www") (WeekDateFormatter.toString wd)
